d237e9eea087025455a0cd8e9948f47b4a499330
andreyblanco <andygblanco@gmail.com>
2023-01-07 06:50:24 -0600

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("");
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {last}, {name} {last}.");
+        Console.WriteLine("");
+
+
     }
 }
