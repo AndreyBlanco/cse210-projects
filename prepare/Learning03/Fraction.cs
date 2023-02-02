f49148532bdcfc1313b0f428f97b274380f2cb0c
andreyblanco <andygblanco@gmail.com>
2023-02-01 21:05:20 -0600

    fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,40 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionString = ($"{_top}/{_bottom}");
+        return fractionString;
+    }
+
+    public float GetDecimalValue()
+    {
+        float fractionDecimal = (float)_top / (float)_bottom;
+        return fractionDecimal;
+    }
+
+    
+
+}
